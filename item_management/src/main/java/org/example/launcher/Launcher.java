a4a65c927ab35155b121c2e86f91367d2ff91682
damithMasterNg <damithanushka95@gmail.com>
2024-01-05 00:16:29 -0800

    Added the back end javaFx main classes

---
+++ item_management/src/main/java/org/example/launcher/Launcher.java
@@ -0,0 +1,15 @@
+package org.example.launcher;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ item_management/src/main/java/org/example/launcher/LauncherWrapper.java
@@ -0,0 +1,7 @@
+package org.example.launcher;
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
